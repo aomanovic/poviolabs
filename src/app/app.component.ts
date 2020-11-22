0e9b8ffc404213523f91bd04237628fbc39832ab
Adnan Omanovic <adnan@evolt.dev>
2020-11-23 00:03:39 +0100

    Service refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import {Component, OnDestroy} from '@angular/core';
-import {Actions, ofActionSuccessful} from '@ngxs/store';
-import {RouterDataResolved} from '@ngxs/router-plugin';
 
 import {Subject} from 'rxjs';
-import {takeUntil} from 'rxjs/operators';
 
 
 @Component({
@@ -15,13 +12,7 @@ export class AppComponent implements OnDestroy {
   title = 'cryptotracker';
   private destroy$ = new Subject<void>();
 
-  constructor(actions$: Actions) {
-    actions$.pipe(
-      ofActionSuccessful(RouterDataResolved),
-      takeUntil(this.destroy$)
-    ).subscribe((action: RouterDataResolved) => {
-      console.log(action);
-    });
+  constructor() {
   }
 
   ngOnDestroy(): void {
